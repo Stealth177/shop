6b9644346f37e742c12633073456a858adbd7a5f
Anton Bukhtin <anton171@yandex.ru>
2013-07-07 15:30:26 +0400

    [fix] carts destroy, update [fix] carts styles [fix] store styles

--- app/models/product.rb
+++ app/models/product.rb
@@ -3,6 +3,8 @@ class Product < ActiveRecord::Base
   
   has_many :line_items
   
+  before_destroy :ensure_not_referenced_by_any_line_item
+  
   validates :title, :description, :image_url, presence: true
 
   validates :price, numericality: {greater_than_or_equal_to: 0.01 }
@@ -16,13 +18,12 @@ class Product < ActiveRecord::Base
 
   private
   
-  #товарные позиции, ссылающиеся на данный товар
-  
   def ensure_not_referenced_by_any_line_item
     if line_items.empty?
       return true
       
-    else errors.add(:base, 'существуют товарные позиции')
+    else 
+      
       return false
     end
   end
